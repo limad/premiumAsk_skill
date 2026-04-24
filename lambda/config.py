910b4cdce34081dca9557d62db32bfdde0a65805
limad <35463455+limad@users.noreply.github.com>
2026-04-24 17:36:54 +0200

    init

---
+++ lambda/config.py
@@ -0,0 +1,5 @@
+# Renseigner les bonnes valeurs
+JEEDOM_URL = "https://xxx.Jeedom.fr/"
+APIKEY = ""
+DEBUG = True # SET TO TRUE IF YOU WANT TO SEE MORE DETAILS IN THE LOGS
+VERIFY_SSL = False # SET TO FALSE IF YOU DO NOT HAVE VALID CERTS OR http
